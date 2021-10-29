f981cdb4e0386d80a5fa8252e52197fa83f0e537
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-29 16:47:23 -0500

    initial redux setup, empty reducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
-import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import logger from 'redux-logger';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// set up the reducer
+const feedback = (state = {}, action) => {
+  return state;
+};
+
+// set up the store
+const store = createStore(
+  combineReducers({
+    feedback,
+  }),
+  applyMiddleware(logger)
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
