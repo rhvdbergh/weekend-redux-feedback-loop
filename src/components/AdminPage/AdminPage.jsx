bca1fa4a659fc4a959877c504da3456b5313ccfc
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-30 09:59:26 -0500

    fetches data and sets to feedbackList

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,6 +1,33 @@
 import { Box, Typography } from '@mui/material';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
+// nothing in this page needs to use redux,
+// since the state isn't shared with any other component
 function AdminPage() {
+  // local state using useState hook
+  const [feedbackList, setFeedbackList] = useState([]);
+
+  // set a useEffect to update the feedbackList on page load
+  useEffect(() => fetchFeedback(), []); // the empty array will make this update only once, after page load
+
+  // fetches latest feedback results from the server
+  const fetchFeedback = () => {
+    // get the data from the server
+    axios
+      .get(`/admin`)
+      .then(({ data }) => {
+        // we're only interested in the data property
+        setFeedbackList(data);
+      })
+      .catch((err) => {
+        console.log(
+          `There was an error retrieving the data from the server:`,
+          err
+        );
+      });
+  };
+
   return (
     <Box>
       <Typography>Hello, admin.</Typography>
