816fd9cbd3fb0bc967cb169abd0d1fdd100c858e
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-10-30 14:50:19 -0500

    adds cors for heroku

--- server/server.js
+++ server/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const cors = require('cors');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('build'));
+
+// set up cors for use on Heroku
+app.use(cors());
 
 /** ---------- EXPRESS ROUTES ---------- **/
 
